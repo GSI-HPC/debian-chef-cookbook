16e1ce381f8b874837c4a236fbfe94e98e167cf2
Lee Hambley <lee.hambley@gmail.com>
2013-07-23 15:59:17 +0200

    Update repo.rb
    The configuration option `key` clashes with [the Ruby method of the same name](http://www.ruby-doc.org/core-2.0/Hash.html#method-i-key). This change switches to hash syntax, vs. object syntax to bypass the errors reported in #1.

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -52,13 +52,13 @@ key_conf ="#{node.debian.repo.path}/key.conf"
 # Public subkey for repository
 repo_key = "#{node.debian.repo.path}/gpg.key"
 # Mail address of key owner
-mail = node.debian.repo.key['Name-Email']
+mail = node.debian.repo['key']['Name-Email']
 
 # Write the configuration file for the GPG key creation
 template key_conf do
   source 'repo_key.conf.erb'
   mode '0600'
-  variables( :conf => node.debian.repo.key )
+  variables( :conf => node.debian.repo['key'] )
   # unless a subkey for the repository had been created already
   not_if do ::File.exists? repo_key end
 end
