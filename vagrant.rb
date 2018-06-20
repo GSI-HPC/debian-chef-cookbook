8ddf0858b06b56316947f0c8aa9f8b577a178a34
Christopher Huhn <C.Huhn@gsi.de>
2018-06-20 10:35:34 +0200

    Add test suite

---
+++ vagrant.rb
@@ -0,0 +1,7 @@
+Vagrant.configure(2) do |config|
+  # install the Debian-provided Chef package
+  config.vm.provision "shell", inline: <<-SHELL
+     sudo apt-get update
+     sudo DEBIAN_FRONTEND=noninteractive apt-get -y install chef
+  SHELL
+end
